f5f4106f28e38ab3790acd16928b6dd365eb925c
KY233466 <zihan.yang@tufts.edu>
2023-09-12 18:24:44 +0800

    clean styling

--- src/views/Mobile/Mobile.js
+++ src/views/Mobile/Mobile.js
@@ -1,11 +1,6 @@
 import { useState } from "react";
-// import { ThemeProvider } from "styled-components";
-// import { ButtonsWrapper, Container, Content, DemoChild } from "./AppStyles";
 import { BottomSheet } from "../../components/BottomSheet/BottomSheet.tsx";
 import React from "react";
-// import { Button } from "./components/Button";
-// import { generatePlaceholderContent } from "./data";
-// import { closedTheme, openTheme } from "./theme";
 
 const Mobile = () => {
   const [isOpen, setIsOpen] = useState(false);
